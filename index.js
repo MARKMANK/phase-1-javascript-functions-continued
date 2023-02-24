ee27d1bd37d2f3d368065b0dffe29d7e7017ed85
kevinmarkman <kevin.w.markman@gmail.com>
2023-02-23 20:50:52 -0600

    Adds functions saturdayFun mondayWork and wrapAdjective

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(summerActivity){
+    if (!summerActivity){
+        summerActivity = 'roller-skate'
+    }
+    return `This Saturday, I want to ${summerActivity}!`
+};
+
+function mondayWork(businessStuff = 'go to the office'){
+    return `This Monday, I will ${businessStuff}.`
+};
+
+function wrapAdjective(razzleDazzle = '*'){
+    return function (whatAmI = 'special'){
+        return `You are ${razzleDazzle}${whatAmI}${razzleDazzle}!`
+    };
+};
